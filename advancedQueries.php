dd05d41645ff7bb4c16bf3c1a1336be695146caf
jewlschmidt <julia.schmidt.tkd@gmail.com>
2018-04-28 22:11:40 -0700

    for some reason DirectorReview is not updating like it did yesterday. Will debug and finish redoing all queries tomorrow.

--- advancedQueries.php
+++ advancedQueries.php
@@ -249,6 +249,7 @@
 
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script type="text/javascript">
+    
     $(function() {
       $("#initType").change(function() {
         $("#retrieve").load("textData/" + $("#initType").val() + $("#interType").val() + ".html");
